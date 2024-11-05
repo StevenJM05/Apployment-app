517279ced9c191dc26185375f20f92e799334e68
ElmerOliva <elmeroliva545@gmail.com>
2024-11-04 22:05:45 -0600

    Configuracion

---
+++ app/src/main/java/sv/edu/itca/apployment/Configuracion.java
@@ -0,0 +1,37 @@
+package sv.edu.itca.apployment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Configuracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_configuracion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    public void acerca(View view) {
+        Intent acerca = new Intent(Configuracion.this, acerca.class);
+        startActivity(acerca);
+    }
+
+    public void politicas(View view) {
+        Intent politica = new Intent(Configuracion.this, politicas.class);
+        startActivity(politica);
+    }
+}
